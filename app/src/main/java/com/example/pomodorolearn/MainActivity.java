6114967c59f852c1f707870c197ef0935e3a87a5
wilderebos <hswastanoe@gmail.com>
2024-11-29 10:31:02 +0700

    add recycler view why podomoro

--- app/src/main/java/com/example/pomodorolearn/MainActivity.java
+++ app/src/main/java/com/example/pomodorolearn/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.pomodorolearn;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdapterFeature adapterModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Inisialisasi daftar model
+        List<model_feature> listModel = new ArrayList<>();
+        listModel.add(new model_feature(
+                "Teknologi Terdepan",
+                "Pomodoro Learn menggabungkan konsep Pomodoro Technique dengan teknologi modern untuk memberikan pengalaman belajar yang terdepan dan efektif."
+        ));
+        listModel.add(new model_feature(
+                "Optimalkan Waktu Belajar",
+                "Dengan menggunakan metode 'Pomodoro', Pomodoro Learn membantu anda mengatur waktu belajar anda secara efisien, memaksimalkan fokus anda dalam sesi-sesi singkat yang terstruktur."
+        ));
+        listModel.add(new model_feature(
+                "Fleksibilitas Pengaturan Waktu",
+                "Pomodoro Learn memungkinkan anda untuk menyesuaikan jangka waktu belajar dan istirahat sesuai dengan preferensi dan kebutuhan anda, memberikan kontrol penuh atas jadwal belajar anda."
+        ));
+        listModel.add(new model_feature(
+                "Nonaktifkan Alarm yang Disesuaikan",
+                "Dapatkan notifikasi alarm yang dapat disesuaikan untuk memandu anda melalui setiap sesi belajar dan istirahat, membantu anda tetap terorganisir dan fokus."
+        ));
+
+        // Inisialisasi Adapter
+        adapterModel = new AdapterFeature(listModel);
+
+        // Inisialisasi RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_feature);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        recyclerView.setAdapter(adapterModel);
     }
 }
